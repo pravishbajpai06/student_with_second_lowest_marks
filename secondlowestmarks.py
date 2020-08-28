d8e46754fbb4b25a4d6ac79f661c1ae7eecf6f35
Pravish Bajpai <55047219+pravishbajpai06@users.noreply.github.com>
2020-08-28 19:30:58 +0530

    Update secondlowestmarks.py

--- secondlowestmarks.py
+++ secondlowestmarks.py
@@ -1,1 +1,18 @@
+l=[]
+    for i in range(int(input())):
+        name = input()
+        score = float(input())
+        l.append([name,score])
+      
+    low=min(b for a,b in l)
+    for a,b in l:
+        l.sort()
+        for a,b in l:
+            if b==low:
+                l.remove([a,b])
 
+        
+    seclow=min(b for a, b in l)
+    for a,b in l:
+        if b==seclow:
+            print(a)        
